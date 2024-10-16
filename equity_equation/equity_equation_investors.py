f93501ae2858a3c15cb354e4495fc1bba3409810
Benjamin Leff <benjamin.w.leff@gmail.com>
2024-10-16 14:33:42 -0500

    fix: removed redundant checks and moved inputs

--- equity_equation/equity_equation_investors.py
+++ equity_equation/equity_equation_investors.py
@@ -103,10 +103,7 @@ class EquityEquation:
             logger.error(msg)
             raise Exception(msg)
 
-        if (
-            estimated_valuation_increase <= Constants.ZERO.value
-            or estimated_valuation_increase >= Constants.ONE_HUNDRED.value
-        ):
+        if estimated_valuation_increase <= Constants.ZERO.value:
             msg = f"""Invalid value for equity percentage.\
                 It should be greater than 0 and less than 100:{estimated_valuation_increase}"""
             logger.error(msg)
@@ -194,63 +191,6 @@ class EquityEquation:
                 "Accept?",
             ]
         )
-
-        total_equity_pool = float(
-            input(
-                """Enter the total equity pool as a
-                                            percentage
-                                            (total_equity_pool, between 0 and 100%): """
-            )
-        )
-
-        proposed_equity_alloc = float(
-            input(
-                """Enter the proposed equity percentage as a
-                                            percentage to be given to the investor as a result
-                                            of their investment in the company
-                                            (proposed_equity_alloc, between 0 and 100%): """
-            )
-        )
-
-        estimated_valuation_increase = float(
-            input(
-                """Enter the estimated increase in valuation of the company
-                                                as a result of the investor investing,
-                                                as a percentage. Said differently,
-                                                how much you expect the valuation of the
-                                                company to inrease after their investment
-                                                (estimated_valuation_increase): """
-            )
-        )
-
-        pre_money_valuation = float(
-            input(
-                """Enter the valuation of the company before the investment (the pre-money valuation)
-                                                (pre_money_valuation): """
-            )
-        )
-
-        # Validate the input parameters
-        if proposed_equity_alloc < Constants.ZERO.value or proposed_equity_alloc >= Constants.ONE_HUNDRED.value:
-            msg = """Invalid value for proposed equity percentage. It should be greater than 0 and less
-                than 100 (excluding 100)."""
-            logger.error(msg)
-            raise ValueError(msg)
-
-        if estimated_valuation_increase <= Constants.ZERO.value:
-            msg = "Invalid value for estimated valuation increase. It should be greater than 0."
-            logger.error(msg)
-            raise ValueError(msg)
-
-        if total_equity_pool <= Constants.ZERO.value or total_equity_pool > Constants.ONE_HUNDRED.value:
-            msg = "Invalid value for estimated valuation increase. It should be greater than 0 and less than 100."
-            logger.error(msg)
-            raise ValueError(msg)
-
-        if pre_money_valuation <= Constants.ZERO.value:
-            msg = "Invalid value for company valuation. It should be greater than 0."
-            logger.error(msg)
-            raise ValueError(msg)
 
         ws["A2"] = "Total Equity Pool"
         ws["B2"] = self.total_equity_pool
@@ -344,9 +284,32 @@ class EquityEquation:
 
 if __name__ == "__main__":
     eeq = EquityEquation(
-        total_equity_pool=100,
-        proposed_equity_alloc=7,
-        estimated_valuation_increase=7.5,
-        pre_money_valuation=20_000_000,
+        total_equity_pool=float(
+            input("""Enter the total equity pool as a percentage (total_equity_pool, between 0 and 100%): """)
+        ),
+        proposed_equity_alloc=float(
+            input(
+                """Enter the proposed equity percentage as a
+                   percentage to be given to the investor as a result
+                   of their investment in the company
+                   (proposed_equity_alloc, between 0 and 100%): """
+            )
+        ),
+        estimated_valuation_increase=float(
+            input(
+                """Enter the estimated increase in valuation of the company
+                   as a result of the investor investing,
+                   as a percentage. Said differently,
+                   how much you expect the valuation of the
+                   company to inrease after their investment
+                   (estimated_valuation_increase): """
+            )
+        ),
+        pre_money_valuation=float(
+            input(
+                """Enter the valuation of the company before the investment (the pre-money valuation)
+                   (pre_money_valuation): """
+            )
+        ),
     )
     eeq()
